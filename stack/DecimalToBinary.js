d479694e5f89a987326fea30e88c0c3950dea659
Alfredo Stanquini Neto <stanquini@gmail.com>
2018-01-08 14:06:30 -0200

    convercao decimal para binario

---
+++ stack/DecimalToBinary.js
@@ -0,0 +1,87 @@
+function Stack() {
+
+    let items = [];
+
+    this.push = function(element){
+        items.push(element);
+    };
+
+    this.pop = function(){
+        return items.pop();
+    };
+
+    this.peek = function(){
+        return items[items.length-1];
+    };
+
+    this.isEmpty = function(){
+        return items.length == 0;
+    };
+
+    this.size = function(){
+        return items.length;
+    };
+
+    this.clear = function(){
+        items = [];
+    };
+
+    this.print = function(){
+        console.log(items.toString());
+    };
+
+    this.toString = function(){
+        return items.toString();
+    };
+}
+ 
+let divideBy2 = (decNumber) => {
+
+    let remStack = new Stack(),
+        rem,
+        binaryString = '';
+
+    while (decNumber > 0){
+        rem = Math.floor(decNumber % 2);
+        remStack.push(rem);
+        decNumber = Math.floor(decNumber / 2);
+    }
+
+    while (!remStack.isEmpty()){
+        binaryString += remStack.pop().toString();
+    }
+
+    return binaryString;
+}
+
+(function(){
+    console.log(divideBy2(233));
+    console.log(divideBy2(10));
+    console.log(divideBy2(1000));
+}());
+
+
+function baseConverter(decNumber, base){
+
+    var remStack = new Stack(),
+        rem,
+        baseString = '',
+        digits = '0123456789ABCDEF';
+
+    while (decNumber > 0){
+        rem = Math.floor(decNumber % base);
+        remStack.push(rem);
+        decNumber = Math.floor(decNumber / base);
+    }
+
+    while (!remStack.isEmpty()){
+        baseString += digits[remStack.pop()];
+    }
+
+    return baseString;
+}
+(function(){
+    console.log(baseConverter(100345, 2));
+    console.log(baseConverter(100345, 8));
+    console.log(baseConverter(100345, 16));
+}());
